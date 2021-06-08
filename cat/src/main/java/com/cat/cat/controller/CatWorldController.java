8fef6501a94ac51463a58aca8f7274fa82293402
gkdmssidhd <75778395+gkdmssidhd@users.noreply.github.com>
2021-06-09 00:35:04 +0900

    06.09 회원관리 기능 수정, CRUD페이지추가

--- cat/src/main/java/com/cat/cat/controller/CatWorldController.java
+++ cat/src/main/java/com/cat/cat/controller/CatWorldController.java
@@ -1,8 +1,10 @@
 package com.cat.cat.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import com.cat.cat.service.CatWorldService;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -48,32 +51,49 @@ public class CatWorldController {
 	public String login() {
 		return "login";
 	}
-	
-	// 실제 로그인한 정보가 존재하는지 디비에서 select하고 그 로그인한 회원의 세션을 부여하는 곳
-	@RequestMapping("loginProc")
-	public String loginProc(CatVO catVO, HttpSession session, Model model) {
-		
-			// 아이디랑 비번 검색해서 가져온거 member에 담기
+
+	  // 실제 로그인한 정보가 존재하는지 디비에서 select하고 그 로그인한 회원의 세션을 부여하는 곳
+	  
+		@RequestMapping("loginProc")
+		public String loginProc(CatVO catVO, HttpSession session, Model model) {
+
+			// 아이디랑 비번 검색해서 가져온거 member에 담기 
 			CatVO member = CatWorldService.loginProc(catVO);
-			
-			// login에서 입력한애들이 DB에 실제로 있는애들인지
+
+			// login에서 입력한애들이 DB에 실제로 있는애들인지 
 			String returnUrl = "";
-			
-			if(member == null) {
+
+			if (member == null) {
 				logger.info("존재하지 않는 회원입니다.");
 				returnUrl = "loginError";
-			} else {
-				// 성공적으로 로그인이 될때 session을 부여
-				session.setAttribute("id", member);   
-				
-				// Model에 담아서 반환
+			} else { // 성공적으로 로그인이 될때 session을 부여 
+				session.setAttribute("id", member);
+
+				// Model에 담아서 반환 
 				model.addAttribute("id", member);
-				
+
 				returnUrl = "loginOk";
 			}
 
-		return returnUrl;
-	}
+			return returnUrl;
+		}
+	
+	/*	
+		login Map으로
+	 * @PostMapping("loginProc") public String loginProc(CatVO catVO,
+	 * HttpServletRequest request)throws Exception {
+	 * 
+	 * HttpSession session = request.getSession();
+	 * 
+	 * Map<String,String> catInform = CatWorldService.loginProc(catVO);
+	 * 
+	 * if(catInform.get("id").equals(catVO.getId()) &&
+	 * catInform.get("password").equals(catVO.getPassword())){
+	 * session.setAttribute("id", catInform.get("id"));
+	 * 
+	 * return "loginOk"; }else{ return "loginError"; } }
+	 */
+	
 	
 	// 로그아웃
 	@RequestMapping("logout")
@@ -92,8 +112,29 @@ public class CatWorldController {
 	public String Catlist(CatVO catVO)  {
 	
 		
-		return "catList";
+		return "catListPage";
 	}
 	
+	// 고양이 분양글을 실제로 올리는 페이지만 리턴하는 곳!
+	@RequestMapping("catRegistPage")
+	public String catRegistPage(CatVO catVO)  {
+		
+		
+		return "catRegistPage";
+	}
 	
+	// 실제로 고양이 분양글을 저장하는 곳
+	@RequestMapping("catSave")
+	public String catSave(CatVO catVO)  {
+		
+		
+		return "catSave";
+	}
+	
+	@RequestMapping("catDetail")
+	public String catDetail(CatVO catVO)  {
+		
+		
+		return "catDetail";
+	}
 }
